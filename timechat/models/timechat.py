89fb5e91fec93b8925edd0afffecd5f334515940
KKKLeon <94350592+KKKLeon@users.noreply.github.com>
2024-05-27 17:32:17 +0800

    Update timechat.py

--- timechat/models/timechat.py
+++ timechat/models/timechat.py
@@ -524,7 +524,7 @@ class TimeChat(Blip2Base):
                                  "https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/blip2_pretrained_flant5xxl.pth")
         img_size = cfg.get("image_size")
         num_query_token = cfg.get("num_query_token")
-        llama_model = cfg.get("llama_model")
+        llama_model = cfg.get("phi2_model")
 
         drop_path_rate = cfg.get("drop_path_rate", 0)
         use_grad_checkpoint = cfg.get("use_grad_checkpoint", False)
